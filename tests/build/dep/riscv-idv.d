d42ed07c9967653ba1ccd90b9b03c9daaef00ac9
angpupp <angpupp@princeton.edu>
2023-05-09 16:47:31 -0400

    LV not working

---
+++ tests/build/dep/riscv-idv.d
@@ -0,0 +1,3 @@
+riscv-idv: ../riscv/riscv-idv.S ../riscv/riscv-macros.h
+
+../riscv/riscv-macros.h:
---
+++ tests/build/dep/riscv-vand.d
@@ -0,0 +1,3 @@
+riscv-vand: ../riscv/riscv-vand.S ../riscv/riscv-macros.h
+
+../riscv/riscv-macros.h:
